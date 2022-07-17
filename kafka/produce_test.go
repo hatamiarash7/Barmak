1024bf9828525243e50fe1fa08859d811625283f
Arash Hatami <hatamiarash7@gmail.com>
2022-07-17 15:53:35 +0430

    add benchmarks

--- kafka/produce_test.go
+++ kafka/produce_test.go
@@ -6,25 +6,56 @@ import (
 )
 
 func TestProduce(t *testing.T) {
-	Segmentio(Message{Key: "Segmentio", Value: "hello world"})
-	Confluent(Message{Key: "Confluent", Value: "hello world"})
-	Sarama(Message{Key: "Sarama", Value: "hello world"})
-	Goka(Message{Key: "Goka", Value: "hello world"})
+	Segmentio([]Message{{Key: "Segmentio", Value: "hello world"}})
+	Confluent([]Message{{Key: "Confluent", Value: "hello world"}})
+	Sarama([]Message{{Key: "Sarama", Value: "hello world"}})
+	Goka([]Message{{Key: "Goka", Value: "hello world"}})
 }
 
-func TestBulkProduce(t *testing.T) {
-	count := 50
+func BenchmarkProduceSegmentio(b *testing.B) {
+	count := b.N
 
-	bulk := make([]Message, count)
+	messages := make([]Message, count)
 
 	for i := 0; i < count; i++ {
-		bulk[i] = Message{Key: "Bulk", Value: fmt.Sprint(i)}
+		messages[i] = Message{Key: "Segmentio", Value: fmt.Sprint(i)}
 	}
 
-	for _, v := range bulk {
-		Segmentio(v)
-		Confluent(v)
-		Sarama(v)
-		Goka(v)
+	Segmentio(messages)
+}
+
+func BenchmarkProduceConfluent(b *testing.B) {
+	count := b.N
+
+	messages := make([]Message, count)
+
+	for i := 0; i < count; i++ {
+		messages[i] = Message{Key: "Confluent", Value: fmt.Sprint(i)}
 	}
+
+	Confluent(messages)
+}
+
+func BenchmarkProduceSarama(b *testing.B) {
+	count := b.N
+
+	messages := make([]Message, count)
+
+	for i := 0; i < count; i++ {
+		messages[i] = Message{Key: "Sarama", Value: fmt.Sprint(i)}
+	}
+
+	Sarama(messages)
+}
+
+func BenchmarkProduceGoka(b *testing.B) {
+	count := b.N
+
+	messages := make([]Message, count)
+
+	for i := 0; i < count; i++ {
+		messages[i] = Message{Key: "Goka", Value: fmt.Sprint(i)}
+	}
+
+	Goka(messages)
 }
