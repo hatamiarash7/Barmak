2fec6757afb2ec8fedf58b9c8aba006877ab91d7
Arash Hatami <hatamiarash7@gmail.com>
2022-07-17 15:54:55 +0430

    use new produce format

--- main.go
+++ main.go
@@ -1,25 +1,27 @@
 package main
 
-import "git.arvan.me/cdn/Barmak/kafka"
+import (
+	"git.arvan.me/cdn/Barmak/kafka"
+)
 
 func main() {
-	kafka.Segmentio(kafka.Message{
+	kafka.Segmentio([]kafka.Message{{
 		Key:   "Segmentio",
 		Value: "hello world",
-	})
+	}})
 
-	kafka.Confluent(kafka.Message{
+	kafka.Confluent([]kafka.Message{{
 		Key:   "Confluent",
 		Value: "hello world",
-	})
+	}})
 
-	kafka.Sarama(kafka.Message{
+	kafka.Sarama([]kafka.Message{{
 		Key:   "Sarama",
 		Value: "hello world",
-	})
+	}})
 
-	kafka.Goka(kafka.Message{
+	kafka.Goka([]kafka.Message{{
 		Key:   "Goka",
 		Value: "hello world",
-	})
+	}})
 }
